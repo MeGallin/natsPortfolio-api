f0446e2c37bac3bd0bd751c28902b7843f977f41
MeGallin <me@garyallin.uk>
2024-08-20 15:26:01 +0100

    Contact info working

--- routes/ContactFormInformationRoutes.js
+++ routes/ContactFormInformationRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   sendContactForm,
+  getContacts,
 } = require('../controllers/ContactFormInformationController');
+const { protect, admin } = require('../middleWare/authMiddleWare');
+
 router.route('/contact-form').post(sendContactForm);
+router.route('/contacts').get(protect, admin, getContacts);
 
 module.exports = router;
